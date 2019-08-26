f50d19bc8b83c32195a221817a3105c5d963cdb3
Randall Knutson <randall@knutson.io>
2019-08-26 13:20:27 -0500

    Fix schema import.

--- src/dbs/index.js
+++ src/dbs/index.js
@@ -1,6 +1,6 @@
 const Database = require('../dbs/Database');
 const Model = require('../dbs/Model');
-const Schema = require('../dbs/Model');
+const Schema = require('../dbs/Schema');
 
 module.exports = {
   Database,
