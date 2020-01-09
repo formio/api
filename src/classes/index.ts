517b78400fc002a4625171f52db3c9bb13ff5d45
Alexander Kiseliov <kiselev20060@gmail.com>
2020-01-09 14:53:19 +0300

    Implemented save as reference feature.

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -2,6 +2,7 @@ export * from './Action';
 export * from './Import';
 export * from './Export';
 export * from './Porter';
+export * from './ReferenceHandler';
 export * from './Resource';
 export * from './Route';
 export * from './Schema';
--- src/entities/Submission/properties/reference.ts
+++ src/entities/Submission/properties/reference.ts
@@ -1,1 +1,28 @@
-export const reference = () => Promise.resolve();
+import {ReferenceHandler} from '../../../classes';
+
+export const reference = (
+  component: any,
+  __data: any,
+  handler: string,
+  action: string,
+  {path, req, res, app}: any,
+) => {
+  const referenceHandler = new ReferenceHandler(component, path, req, res, app);
+
+  if (handler !== 'afterValidate' && handler !== 'afterActions') {
+    return Promise.resolve();
+  }
+
+  switch (action) {
+    case 'get':
+      return referenceHandler.onGet(handler);
+    case 'post':
+      return referenceHandler.onPost(handler);
+    case 'put':
+      return referenceHandler.onPut(handler);
+    case 'index':
+      return referenceHandler.onIndex(handler);
+    default:
+      return Promise.resolve();
+  }
+};
