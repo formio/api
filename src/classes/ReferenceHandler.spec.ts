0b41d10134e1a5b6aa5d1ee37c85dc0943e10e85
Alexander Kiseliov <kiselev20060@gmail.com>
2020-01-13 12:23:37 +0300

    ReferenceHandler tests refactoring.

--- src/classes/ReferenceHandler.spec.ts
+++ src/classes/ReferenceHandler.spec.ts
@@ -1,12 +1,12 @@
 import {assert, expect} from 'chai';
 import * as sinon from 'sinon';
 
-import {lodash as _} from '../util/lodash';
-import { ReferenceHandler } from './ReferenceHandler';
-
 import {Express} from '../../test/mocks/Express';
 import {Database} from '../dbs/Database';
 import {Api} from '../FormApi';
+import {lodash as _} from '../util/lodash';
+
+import { ReferenceHandler } from './ReferenceHandler';
 
 const router: any = new Express();
 const db = new Database();
@@ -16,34 +16,28 @@ const component = {multiple: false};
 const path = 'form';
 const req = {};
 
-const mockId = app.db.toID('id');
-const mockAnotherId = app.db.toID('id2');
+const mockIds = [app.db.toID('id'), app.db.toID('id2'), app.db.toID('id3')];
+
+const mockItems = mockIds.map((id) => ({
+  data: {
+    [path]: {_id: id},
+  },
+}));
 
 const res = {
   resource: {
+    item: mockItems[0],
+    items: mockItems,
+  },
+};
+
+const multipleComponentRes = {
+  resource: {
     item: {
       data: {
-        [path]: {
-          _id: mockId,
-        },
+        [path]: mockIds.map((_id) => ({_id})),
       },
     },
-    items: [
-      {
-        data: {
-          [path]: {
-            _id: mockId,
-          },
-        },
-      },
-      {
-        data: {
-          [path]: {
-            _id: mockAnotherId,
-          },
-        },
-      },
-    ],
   },
 };
 
@@ -61,7 +55,7 @@ describe('Reference Handler', () => {
   });
 
   describe('Handles Get', () => {
-    const mockIdQuery = {_id: mockId};
+    const mockIdQuery = {_id: mockIds[0]};
 
     let getIdQueryStub: sinon.SinonStub;
     let loadReferencesStub: sinon.SinonStub;
@@ -88,26 +82,57 @@ describe('Reference Handler', () => {
       assert(loadReferencesStub.notCalled);
     });
 
-    it('Sets the first item to resource value if items were found', async () => {
-      const mockItem = {_id: mockId, textField: 'test'};
+    describe('Resource value', () => {
+      it('Sets correctly if items were found', async () => {
+        const mockItem = {_id: mockIds[0], textField: 'test'};
 
-      getIdQueryStub.returns(mockIdQuery);
-      loadReferencesStub.resolves([mockItem]);
+        getIdQueryStub.returns(mockIdQuery);
+        loadReferencesStub.resolves([mockItem]);
 
-      await instance.onGet(afterActionsHandler);
+        await instance.onGet(afterActionsHandler);
 
-      expect((instance as any).res.resource.item.data[path])
-        .to.deep.equal(mockItem);
+        expect((instance as any).res.resource.item.data[path])
+          .to.deep.equal(mockItem);
+      });
+
+      it('Sets correctly if items were not found', async () => {
+        getIdQueryStub.returns(mockIdQuery);
+        loadReferencesStub.resolves([]);
+
+        await instance.onGet(afterActionsHandler);
+
+        expect((instance as any).res.resource.item.data[path])
+          .to.deep.equal(res.resource.item.data[path]);
+      });
     });
 
-    it('Sets resource value as it was if items were not found', async () => {
-      getIdQueryStub.returns(mockIdQuery);
-      loadReferencesStub.resolves([]);
+    describe('References', () => {
+      it('Loads correctly with multiple component', async () => {
+        const multipleComponent = _.cloneDeep(component);
+        multipleComponent.multiple = true;
 
-      await instance.onGet(afterActionsHandler);
+        const request = _.cloneDeep(req);
+        const response = _.cloneDeep(multipleComponentRes);
 
-      expect((instance as any).res.resource.item.data[path])
-        .to.deep.equal(res.resource.item.data[path]);
+        const expectedIds = {$in: mockIds};
+
+        instance = new ReferenceHandler(multipleComponent, path, request, response, app);
+        loadReferencesStub = sinon.stub(instance, 'loadReferences' as any);
+
+        await instance.onGet(afterActionsHandler);
+
+        expect(loadReferencesStub.args[0][0]._id).to.eql(expectedIds);
+
+        loadReferencesStub.restore();
+      });
+
+      it('Loads correctly with not multiple component', async () => {
+        getIdQueryStub.restore();
+        await instance.onGet(afterActionsHandler);
+
+        expect(loadReferencesStub.args[0][0]._id)
+          .to.eql(res.resource.item.data[path]._id);
+      });
     });
   });
 
@@ -126,48 +151,60 @@ describe('Reference Handler', () => {
     });
 
     describe('Handles Post', () => {
-      it('Sets resource to request if handler is afterValidate', async () => {
-        await instance.onPost(afterValidateHandler);
+      describe('afterValidate', () => {
+        it('Sets resource to request', async () => {
+          await instance.onPost(afterValidateHandler);
 
-        assert(setResourceToRequestStub.called);
-        assert(getResourceFromRequest.notCalled);
+          assert(setResourceToRequestStub.called);
+          assert(getResourceFromRequest.notCalled);
+        });
       });
 
-      it('Gets resource from request if handler is afterActions', async () => {
-        await instance.onPost(afterActionsHandler);
+      describe('afterActions', () => {
+        it('Gets resource from request', async () => {
+          await instance.onPost(afterActionsHandler);
 
-        assert(setResourceToRequestStub.notCalled);
-        assert(getResourceFromRequest.called);
+          assert(setResourceToRequestStub.notCalled);
+          assert(getResourceFromRequest.called);
+        });
       });
 
-      it('Doesn\'t invoke anything if handler is not afterValidate or afterActions', async () => {
-        await instance.onPost('test');
+      describe('Neither afterValidate or afterActions', () => {
+        it('Doesn\'t invoke anything', async () => {
+          await instance.onPost('test');
 
-        assert(setResourceToRequestStub.notCalled);
-        assert(getResourceFromRequest.notCalled);
+          assert(setResourceToRequestStub.notCalled);
+          assert(getResourceFromRequest.notCalled);
+        });
       });
     });
 
     describe('Handles Put', () => {
-      it('Sets resource to request if handler is afterValidate', async () => {
-        await instance.onPut(afterValidateHandler);
+      describe('afterValidate', () => {
+        it('Sets resource to request', async () => {
+          await instance.onPut(afterValidateHandler);
 
-        assert(setResourceToRequestStub.called);
-        assert(getResourceFromRequest.notCalled);
+          assert(setResourceToRequestStub.called);
+          assert(getResourceFromRequest.notCalled);
+        });
       });
 
-      it('Gets resource from request if handler is afterActions', async () => {
-        await instance.onPut(afterActionsHandler);
+      describe('afterActions', () => {
+        it('Gets resource from request', async () => {
+          await instance.onPut(afterActionsHandler);
 
-        assert(setResourceToRequestStub.notCalled);
-        assert(getResourceFromRequest.called);
+          assert(setResourceToRequestStub.notCalled);
+          assert(getResourceFromRequest.called);
+        });
       });
 
-      it('Doesn\'t invoke anything if handler is not afterValidate or afterActions', async () => {
-        await instance.onPut('test');
+      describe('Neither afterValidate or afterActions', () => {
+        it('Doesn\'t invoke anything', async () => {
+          await instance.onPut('test');
 
-        assert(setResourceToRequestStub.notCalled);
-        assert(getResourceFromRequest.notCalled);
+          assert(setResourceToRequestStub.notCalled);
+          assert(getResourceFromRequest.notCalled);
+        });
       });
     });
   });
@@ -191,7 +228,7 @@ describe('Reference Handler', () => {
       assert(lodashGetStub.notCalled);
     });
 
-    it('Doesn\'t set items to resource value if items were not found', async () => {
+    it('Doesn\'t set items to resource value if they were not found', async () => {
       lodashGetStub.returns(null);
       await instance.onIndex(afterActionsHandler);
 
@@ -200,24 +237,14 @@ describe('Reference Handler', () => {
     });
 
     it('Sets items to resource value if they were found', async () => {
-      const itemsMock = [
-        {
-          data: {
-            [path]: {
-              _id: mockId,
-              textField: 'textField1',
-            },
+      const itemsMock = mockIds.map((_id) => ({
+        data: {
+          [path]: {
+            _id,
+            textField: `textField${_id}`,
           },
         },
-        {
-          data: {
-            [path]: {
-              _id: mockAnotherId,
-              textField: 'textField2',
-            },
-          },
-        },
-      ];
+      }));
 
       lodashSetStub.restore();
       lodashGetStub.returns(itemsMock);
@@ -226,11 +253,7 @@ describe('Reference Handler', () => {
 
       await instance.onIndex(afterActionsHandler);
 
-      const items = (instance as any).res.resource.items;
-
-      expect(items.length).to.equal(2);
-      expect(items).to.deep.include(itemsMock[0]);
-      expect(items).to.deep.include(itemsMock[1]);
+      expect((instance as any).res.resource.items).to.eql(itemsMock);
 
       flatItemStub.restore();
     });
